f2f6d43ded0873f7bb1b13439adc661ca79430cb
christopherho96 <ho.christopher.tak@gmail.com>
2018-01-03 22:11:38 -0500

    learned nesting methods in Vue

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ new Vue({
     //looks for element with id of 'app' 
     el: '#app',
     data: {
-        name: 'Chris Ho'
+        name: 'Chris Ho',
+        job: 'Ninja'
+    },
+    methods:{
+        greet1: function(){
+            return 'Good Morning you ugly'
+        },
+        greet2: function(greetingWord){
+            return greetingWord
+        },
+        sentence: function(){
+            return this.name + " is a " + this.job
+        }
+        
     }
 });

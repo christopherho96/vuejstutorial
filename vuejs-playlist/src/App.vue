b714f4c5e1788876beedc05a5e60ac7abf609943
christopherho96 <ho.christopher.tak@gmail.com>
2018-01-04 14:42:06 -0500

    using vue CLI with components and nesting

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <ninja></ninja>
+  </div>
+</template>
+
+<script>
+
+import Ninja from './Ninja.vue'
+export default {
+  components:{
+    'ninja' : Ninja
+  },
+  data () {
+    return {
+      title: 'Ninja App'
+    }
+  }
+}
+</script>
+
+<!-- the scoped keyword will only apply the styles to the contents of this file-->
+<style scoped>
+h1{
+   color: purple;
+}
+</style>

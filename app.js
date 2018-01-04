94518a0616edc3f3810542b655a39edf1e2057ca
christopherho96 <ho.christopher.tak@gmail.com>
2018-01-04 09:50:03 -0500

    learn event listeners, modifiers, event binding

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ new Vue({
     el: '#app',
     data: {
         name: 'Chris Ho',
-        job: 'Ninja'
+        job: 'Ninja',
+        website : 'https:twitter.com',
+        websiteTag: '<a href = "https:twitter.com"> Twitter Link 2</a>',
+        age: '21',
+        x: 0,
+        y: 0
     },
     methods:{
         greet1: function(){
@@ -14,6 +19,21 @@ new Vue({
         },
         sentence: function(){
             return this.name + " is a " + this.job
+        },
+        add: function(inc){
+            this.age += inc
+        },
+        minus: function(dec){
+            this.age -= dec
+        },
+        updateXY: function(event){
+            console.log(event)
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
+
+        click: function(){
+            alert("You clicked me")
         }
         
     }

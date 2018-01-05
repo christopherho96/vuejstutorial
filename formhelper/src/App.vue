d2d3faa38f96ade69626b88acfe7b94991fc44e6
christopherho96 <ho.christopher.tak@gmail.com>
2018-01-05 10:29:31 -0500

    dynamic components

--- formhelper/src/App.vue
+++ formhelper/src/App.vue
@@ -1,30 +1,28 @@
 <template>
   <div>
-    <form-Helper>
-      <div slot = "form-header">
-        <h3>This is the title of the form</h3>
-        <p>Information about the form</p>
-      </div>
-      <div slot = "form-fields">
-        <input type = "text" placeholder=  "name" required/>
-        <input type = "password" placeholder=  "password" required/>
-      </div>
-      <div slot = "form-controls">
-        <button v-on:click= "handleSubmit">Click me</button>
-      </div>
-    </form-Helper>
+    <!-- keep-alive tag specific to vue.js syntax, it says DONT YOU DARE DELETE the stuff within the tag -->
+    <keep-alive>
+      <component v-bind:is = "component"></component>
+    </keep-alive>
+    <button v-on:click = "component = 'form-one'">Show form one</button>
+    <button v-on:click = "component = 'form-two'">Show form two</button>
   </div>
 </template>
 
 <script>
 import formHelper from './components/formHelper.vue'
+import formOne from './components/formOne.vue'
+import formTwo from './components/formTwo.vue'
+
 export default {
   components:{
-    'form-Helper': formHelper
+    'form-Helper': formHelper,
+    'form-one': formOne,
+    'form-two': formTwo
   },
   data () {
     return {
-      title: 'I am a dynamic slot title'
+      component: 'form-two'
     }
   },
   methods:{
